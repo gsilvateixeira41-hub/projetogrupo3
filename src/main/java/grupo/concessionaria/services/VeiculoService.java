80d34fcc0fa92fa67e29c53a22bdcc94665bde7e
gustavo teixeira <gsilvateixeira41@gmail.com>
2025-11-19 09:17:11 -0300

    meu primeiro commit

---
+++ src/main/java/grupo/concessionaria/services/VeiculoService.java
@@ -0,0 +1,41 @@
+package grupo.concessionaria.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import grupo.concessionaria.entities.Veiculo;
+import grupo.concessionaria.repositories.VeiculoRepository;
+
+@Service
+public class VeiculoService {
+	
+@Autowired
+private VeiculoRepository repository;
+
+public List<Veiculo> procurarTodos(){
+	return repository.findAll();
+}
+public Veiculo procurarPorId (Integer id) {
+	return repository.findById(id).get();
+}
+public String adicionarVeiculo(Veiculo veiculo) {
+		repository.save (veiculo);
+		return "Véiculo alugado com sucesso";
+}
+public String editarVeiculo(Integer id,Veiculo veiculo){ 
+		Veiculo response= repository.findById (id).get();
+		response.setId(veiculo.getId());
+		response.setModelo(veiculo.getModelo());
+		response.setPreco(veiculo.getPreco());
+		response.setCor(veiculo.getCor());
+		repository.save(response);
+		return" Veiculo alugado com sucesso!";		
+}
+public void excluirVeiculo(Integer id) {
+	Veiculo response = repository.findById(id).get();
+	repository.delete(response);
+	}
+
+}
